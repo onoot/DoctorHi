11afeb77b52d58e4329f2772fc9a5242a15c2d14
onoot <penisxuei@gmail.com>
2025-08-30 16:02:50 +0700

    sadsad

--- src/routes/transactionRoutes.mjs
+++ src/routes/transactionRoutes.mjs
@@ -2,6 +2,7 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { mkdirSync } from 'fs'; // Импортируем mkdirSync напрямую из fs
